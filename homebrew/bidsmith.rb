2f550144aef6e8516638f5d80ccc2e48e202d0c3
Krzysztof Modras <chrmod@chrmod.net>
2026-05-20 14:13:30 +0200

    Pin homebrew formula SHAs for v0.1.1

--- homebrew/bidsmith.rb
+++ homebrew/bidsmith.rb
@@ -7,22 +7,22 @@ class Bidsmith < Formula
   on_macos do
     on_arm do
       url "https://github.com/chrmod/bidsmith/releases/download/v#{version}/bidsmith-aarch64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_SHA256_AARCH64_APPLE_DARWIN"
+      sha256 "c421f631af093c102d16824f7abbb2b51cfd92a22c2e7644a09b0089aa22b0fb"
     end
     on_intel do
       url "https://github.com/chrmod/bidsmith/releases/download/v#{version}/bidsmith-x86_64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_SHA256_X86_64_APPLE_DARWIN"
+      sha256 "eb14e79f5545c52d5a77f2f2ea412c3e35d1f9054476a41e0d74d91904012c26"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/chrmod/bidsmith/releases/download/v#{version}/bidsmith-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "REPLACE_WITH_SHA256_AARCH64_UNKNOWN_LINUX_GNU"
+      sha256 "8613fcd5970ad171a4021dd7547829608b3e8ad18176a636febdbef78114f5c2"
     end
     on_intel do
       url "https://github.com/chrmod/bidsmith/releases/download/v#{version}/bidsmith-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "REPLACE_WITH_SHA256_X86_64_UNKNOWN_LINUX_GNU"
+      sha256 "c35e4c545645eb9b328b4e996766f6a33aaeb9767a7bb362a89f209da0492ed0"
     end
   end
 
